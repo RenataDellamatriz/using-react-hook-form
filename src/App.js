a00279316392ed3e595b0856cc3a4f0fad7ab5a6
Renata Dellamatriz <r.dellamatriz@hotmail.com>
2022-10-30 00:35:59 -0300

    Learnign how to use React Hook Form

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "./global.css";
+
+import { useForm } from "react-hook-form";
 
 function App() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const addPost = (values) => console.log(values);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <div className="card-post">
+        <h1>Formulário teste</h1>
+
+        <div className="line-post"></div>
+
+        <div className="card-body-post">
+          <form onSubmit={handleSubmit(addPost)}>
+            <div className="fields">
+              <label>Título</label>
+              <input
+                type="text"
+                name="title"
+                {...register("title", { required: true })}
+              />
+              {errors.title && <span>This field is required</span>}
+            </div>
+
+            <div className="fields">
+              <label>Descrição</label>
+              <input
+                type="text"
+                name="description"
+                {...register("description")}
+              />
+            </div>
+
+            <div className="fields">
+              <label>Conteúdo</label>
+              <textarea
+                type="text"
+                name="content"
+                {...register("content")}
+              ></textarea>
+            </div>
+
+            <div className="btn-post">
+              <button type="submit">Enviar</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </main>
   );
 }
 
